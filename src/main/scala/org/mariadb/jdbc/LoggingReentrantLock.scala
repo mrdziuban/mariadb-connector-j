11e757546aca1891f6892f660c9b5f1fb7094ebc
Matt Dziuban <mrdziuban@gmail.com>
2022-01-07 15:08:40 -0500

    Add time to debug log.

--- src/main/scala/org/mariadb/jdbc/LoggingReentrantLock.scala
+++ src/main/scala/org/mariadb/jdbc/LoggingReentrantLock.scala
@@ -1,12 +1,16 @@
 package org.mariadb.jdbc
 
-import io.circe.{Json, Printer}
+import io.circe.{Encoder, Json, Printer}
 import io.circe.syntax._
 import java.sql.Connection
+import java.time.{Instant, ZoneId}
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.locks.ReentrantLock
 import org.mariadb.jdbc.internal.logging.{Logger, LoggerFactory}
 
 class LoggingReentrantLock extends ReentrantLock {
+  import LoggingReentrantLock._
+
   private var connection: Connection = _
 
   final def setConnection(conn: Connection): Unit = {
@@ -31,6 +35,7 @@ class LoggingReentrantLock extends ReentrantLock {
 
   private def logDebug(methodCalled: String): Unit =
     logger.debug(jsonPrinter.print(Json.obj(
+      "time" := Instant.now,
       "connection" := Option(connection).map(c => Json.obj(
         "hashCode" := connection.hashCode,
       )),
@@ -52,4 +57,9 @@ class LoggingReentrantLock extends ReentrantLock {
     logDebug("unlock")
     super.unlock()
   }
+}
+
+object LoggingReentrantLock {
+  private val instantFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZone(ZoneId.systemDefault())
+  implicit val instantEncoder: Encoder[Instant] = Encoder[String].contramap(instantFormatter.format)
 }
